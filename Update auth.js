3267cd19672de6e084b2e9d5202c10b909276073
Chozenalpha <ronaldowino716@gmail.com>
2026-04-15 17:51:39 +0300

    Implement user registration and login routes

---
+++ Update auth.js
@@ -0,0 +1,34 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+// REGISTER
+router.post("/register", async (req, res) => {
+  const hashed = await bcrypt.hash(req.body.password, 10);
+
+  const user = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: hashed
+  });
+
+  await user.save();
+  res.json(user);
+});
+
+// LOGIN
+router.post("/login", async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) return res.status(404).json("User not found");
+
+  const valid = await bcrypt.compare(req.body.password, user.password);
+  if (!valid) return res.status(400).json("Wrong password");
+
+  const token = jwt.sign({ id: user._id }, "SECRET_KEY");
+
+  res.json({ token, user });
+});
+
+module.exports = router;
